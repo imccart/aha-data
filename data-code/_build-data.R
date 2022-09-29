6342f7855df774deae0bd7959acd7f1b35cbe1ec
won-jun-choi <choi152552@gmail.com>
2022-09-29 14:38:41 -0400

    merged until 2011

--- data-code/_build-data.R
+++ data-code/_build-data.R
@@ -2,10 +2,63 @@
 ## Title:         Hospital Landscape Data from AHA
 ## Author:        Ian McCarthy, Wonjun Choi
 ## Date Created:  9/2/2022
-## Date Edited:   9/2/2022
-
+## Date Edited:   9/29/2022
 
 # Preliminaries -----------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(data.table, tidyverse, janitor)
+pacman::p_load(data.table, tidyverse, janitor, here)
 
+# Load full list of hospitals --------------------------------------------
+# working directory is {}/aha-data/ (above data-code)
+df_hosp <- tibble()
+for (y in 2007:2019){
+  if (y == 2007) {
+    f <- here("data",'input',paste0('AHA FY ',y),'COMMA','comma.csv')
+  } else if (y==2008) {
+    f <- here('data','input',paste0('AHA FY ',y),'COMMA','pubas08.csv')
+  } else if (y==2009) {
+    f <- here('data','input',paste0('AHA FY ',y),'Comma','pubas09.csv')
+  } else if (y<=2012) {
+    f <- here('data','input',paste0('AHA FY ',y),
+              'COMMA', paste0('ASPUB',y-2000,'.csv'))
+  } else if (y<=2015) {
+    f <- here('data','input',paste0('AHA FY ',y),
+              'COMMA', paste0('ASPUB',y-2000,'.CSV'))
+  } else {
+    f <- here('data','input',paste0('AHA FY ',y),
+              paste0('ASPUB',y-2000,'.csv'))
+  }
+  df_temp <- read.csv(f)
+  df_temp <- df_temp %>% select('ID','MNAME') %>% mutate(year=y)
+  df_hosp <- rbind(df_hosp, df_temp)
+}
+df_hosp <- df_hosp %>% group_by(ID) %>% 
+  mutate(n = n()) %>% ungroup()
+
+# Find changes/reasons --------------------------------------------------
+
+# Outline of the current data-building scheme
+# 1. Make a full (long*) list of hosp-year (done above: df_hosp)
+#    * what's better: long? wide?
+#    !! should be year by year (otherwise, left_join is troublesome)
+# 1. Subset the full-list
+# 2. For each year, create 2 dataframes: for addition, deletion
+# 3. For addition, create a dummy for each reason, but drop status/Status
+# 4. left_join 3 into df_hosp using (ID,year)*
+#    * we also have state, city, hospital name
+# 5. For deletion, create a dummy, but drop status/Status
+# 6. add (NOT left_join) ID,year to df_hosp with reason
+# 6. If del. reason is not hospital, delete ALL OF THE OBSERVATIONS
+# 7. If demerged/merged/etc, from where?
+# 8. Final list??
+
+aha_data <- tibble()  # modify this later
+for (y in 2007:2011) {
+  source(paste0('data-code/add-change-y.R'))
+  aha_data <- bind_rows(aha_data, df_year)
+}
+
+# Code for: If demerged, from where?
+#   df_temp <- df %>% filter(demerged == 1) %>%
+#     select(ID, REASON.FOR.ADDITION)
+#   df_temp$demerged_from = lapply(df_temp$REASON.FOR.ADDITION, spl_last)
---
+++ data-code/add-change-y.R
@@ -0,0 +1,61 @@
+# Meta ---------------------------------------------------------
+# Author: Wonjun Choi
+# Date: Sept-29-2022
+# Description: A file to add a reason of change in 2007
+#
+# load change data ------------------------------------------------
+#y <- 2008  # y comes from the global variable (fix it later)
+print(y)
+df_year <- df_hosp %>% filter(year==y)
+df_reg_add <- read.csv(paste0("data/temp/change_reg_add_",y,".csv"))
+df_reg_del <- read.csv(paste0("data/temp/change_reg_del_",y,".csv"))
+df_nonreg_add <- read.csv(paste0("data/temp/change_nonreg_add_",y,".csv"))
+df_nonreg_del <- read.csv(paste0("data/temp/change_nonreg_del_",y,".csv"))
+
+# functions ------------------------------------------------------
+spl <- function(text) {
+  li <- str_split(text, " ")
+  return(li[[1]])
+}
+spl_first <- function(text) spl(text)[[1]]
+spl_last <- function(text) tail(spl(text),n=1)
+
+# addition ----------------------------------------------------------
+df_add <- rbind(df_reg_add, df_nonreg_add)  # reg status doesn't matter
+df_add <- df_add %>%
+  mutate(ID = as.character(ID), add=1,
+         reason = REASON.FOR.ADDITION)
+df_add$reason_head <- lapply(df_add$reason, spl_first)
+
+df_add <- df_add %>%  # create dummies for reason_head
+  filter(reason_head != 'Status',
+         reason_head != 'status') %>%
+  mutate(dummy=1, readum = reason_head) %>% 
+  spread(readum, value=dummy, sep='_')
+
+df_add <- df_add %>%  # update to df_hosp
+  select(ID, add, reason, reason_head, starts_with('readum_')) %>%
+  mutate(year = y)
+df_year <- df_year %>%
+  left_join(df_add, by=c('ID','year'), suffix=c("",".z"))
+
+# deletion ------------------------------------------------------
+df_del <- rbind(df_reg_del, df_nonreg_del)
+df_del$reason_head <- lapply(df_del$REASON.FOR.DELETION, spl_first)
+df_del <- df_del %>%
+  mutate(ID = as.character(ID),
+         del = 1, 
+         reason = REASON.FOR.DELETION,
+         year = y)
+
+df_del <- df_del %>%  # create dummies for reason_head
+  filter(reason_head != 'Status') %>%
+  mutate(dummy=1,
+         readum = reason_head) %>% 
+  spread(readum, value=dummy, sep='_')  
+
+df_del <- df_del %>%  # update to df_hosp
+  select(ID, del, reason, reason_head, starts_with('readum_')) %>%
+  mutate(year = y)
+
+df_year <- bind_rows(df_year, df_del)
